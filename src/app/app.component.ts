d0c3f56b3527a13bb1cfb7e0713473862d1557c7
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-05-09 07:09:24 +0530

    setting up layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MusicVAE, Player } from '@magenta/music/es6';
+import { SidePanelComponent } from './layout/side-panel/side-panel.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    SidePanelComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
